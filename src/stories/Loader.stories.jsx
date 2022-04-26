941235331ae46a536116349e21b7f3f74457bf89
phucnguyen1998 <phucnguyen11011998@gmail.com>
2022-04-26 16:39:18 +0700

    preview component with storybook

---
+++ src/stories/Loader.stories.jsx
@@ -0,0 +1,8 @@
+import Loader from "../components/common/Loader"
+
+export default {
+  title: 'Loader',
+  component: Loader
+}
+
+export const LoaderWithLogo = () => <Loader includedLogo />
---
+++ src/stories/Logo.stories.jsx
@@ -0,0 +1,8 @@
+import Logo from './../components/common/Logo'
+
+export default {
+  title: 'Logo',
+  component: Logo
+}
+
+export const LogoApp = () => <Logo />
